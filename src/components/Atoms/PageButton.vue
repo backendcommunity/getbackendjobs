85c8f07eac0c901492d5c38f892b357d95772c8d
Nweke kenechukwu christopher <72747787+icodejsx@users.noreply.github.com>
2023-12-27 20:31:15 +0100

    somethingworking well

---
+++ src/components/Atoms/PageButton.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export default {
+        name:'PageButton'
+    }
+</script>
+
+
+<template>
+    <button>
+        Register now
+    </button>
+</template>
---
+++ src/components/Molecules/PageHeader.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import PageButton from '../Atoms/PageButton.vue'
+export default {
+    name: 'Header',
+    components: {
+     PageButton
+    }
+}
+</script>
+
+<template>
+    <div>
+        <img  src="@/assets/logo.svg" alt="logo" />
+        <PageButton/>
+        
+     
+    </div>
+</template>
