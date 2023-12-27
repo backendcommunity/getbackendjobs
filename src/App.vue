c83c2195731ae261bff9b14b2e1ba94a8579debd
Nweke kenechukwu christopher <72747787+icodejsx@users.noreply.github.com>
2023-12-27 17:23:33 +0100

    learing  view js newly

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+   <h1 class="text-3xl text-red-600"> i am learning view </h1>
+  </header>
+
+  <RouterView />
+</template>
+
