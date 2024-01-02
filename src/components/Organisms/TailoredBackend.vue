d508c67b1636675445ec0ad3e5cee5755fda4b0d
Nweke kenechukwu christopher <72747787+icodejsx@users.noreply.github.com>
2024-01-02 21:15:23 +0100

    yeah stillw working ont it

--- src/components/Organisms/TailoredBackend.vue
+++ src/components/Organisms/TailoredBackend.vue
@@ -11,10 +11,10 @@ export default {
 </script>
 
 <template>
-    <div class="bg-gray-100 pt-20  my-40">
+    <div class="bg-gray-100 pt-20  p-6 md:my-40">
         <div class=" bg-gray-100 ">
             <div class="flex flex-col gap-6 justify-center items-center">
-                <h1 class="text-5xl font-bold text-secondaryColor">Tailored Backend Jobs</h1>
+                <h1 class="md:text-5xl  text-4xl font-bold text-secondaryColor">Tailored Backend Jobs</h1>
                 <p class="text-xl text-gray-400">Spend less time searching. More time applying. Our bot curate only the
                     finest
                 </p>
@@ -22,7 +22,7 @@ export default {
 
         </div>
 
-        <div class="grid grid-cols-4 px-24 py-16  gap-10">
+        <div class="grid lg:grid-cols-4 md:grid-cols-2 lg:px-24 py-16  gap-10">
             <JobBox image='@/assets/cac.svg' text1="DevOps " text2="Engineer" numer="180k" />
             <JobBox text1="Php Laravel " text2="Developer" numer="180k" />
             <JobBox image='@/assets/icon3.svg' text1="Design & " text2="Develoment" numer="400k" />
